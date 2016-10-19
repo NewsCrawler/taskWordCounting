f5228b7ba6746845c728aac3e645922341cfb4ab
Hyunjun529 <hyunjun529@gmail.com>
2016-10-19 18:39:09 +0900

    init project

---
+++ wordcounter/src/main/java/net/p316/newscrawler/wordcounter/App.java
@@ -0,0 +1,13 @@
+package net.p316.newscrawler.wordcounter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
