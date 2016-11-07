9d57453a0e4ad5a5ea8130b418ec1826e2a58e75
Hyunjun529 <hyunjun529@gmail.com>
2016-11-07 15:53:26 +0900

    Hello World!

---
+++ newscrawler/src/main/java/net/p316/news/newscrawler/App.java
@@ -0,0 +1,13 @@
+package net.p316.news.newscrawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
